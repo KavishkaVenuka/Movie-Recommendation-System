0f7406c8c7837f12a4064115d1d97fa9fff1b6a2
kavishka venuka <kavishkavenuka554pro@gmail.com>
2025-09-18 23:32:19 +0530

    Created the backend server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,13 @@
 import Express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = Express();
+
+const PORT = process.env.PORT  ||5000;
+const HOST = process.env.HOST|| "127.0.0.1";
+
+const server = app.listen(PORT, HOST, (req,res) => {
+        console.log(`server is running on ${server.address().address}. port : ${server.address().port}`);
+})
