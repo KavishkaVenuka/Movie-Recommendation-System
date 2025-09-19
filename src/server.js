079bf21ae3cab2bad63f95fd94e7f325b3fa2dc8
kavishka venuka <kavishkavenuka554pro@gmail.com>
2025-09-19 13:39:18 +0530

    Exported the server and created the first route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import Express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js"
 
 dotenv.config();
 
@@ -8,6 +9,10 @@ const app = Express();
 const PORT = process.env.PORT  ||5000;
 const HOST = process.env.HOST|| "127.0.0.1";
 
+app.use("/api/user/auth", authRoutes);
+
 const server = app.listen(PORT, HOST, (req,res) => {
         console.log(`server is running on ${server.address().address}. port : ${server.address().port}`);
 })
+
+export default server;
